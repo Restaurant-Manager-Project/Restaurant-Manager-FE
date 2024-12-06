4803ff15aede70c8da50524860f1b08788535579
MPIO1002 <minhphuc309@gmail.com>
2024-12-06 17:18:52 +0700

    lastest deploy

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,9 +8,6 @@ export default defineConfig({
     include: [], 
   },
   server: {
-    host: '0.0.0.0', // Lắng nghe trên mọi IP
-    port: process.env.PORT || 3000, // Sử dụng cổng mà Render cấp (hoặc cổng mặc định là 3000)
     historyApiFallback: true,
   },
-});
-
+})
